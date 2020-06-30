e026d5e78db1f6aac5eae98be9c0abd28da58bb9
tbone7771 <47194216+tbone7771@users.noreply.github.com>
2020-06-29 21:35:16 -0400

    Update guess the number.py

--- guess the number.py
+++ guess the number.py
@@ -1,48 +1,61 @@
 import random
 import time
-p = 100 ##starting points, optional input
-n = random.randint(1, p) ## picks the random number
-m = 1 ##starting multiplier
+repeat = True
 d = .1 ##difficulty
-
-
 print("Welcome to Guess the Number!")
 time.sleep(2)
-print("You will get points based on how many guesses it takes you to guess the number")
+print("You will get points based on how many guesses it takes you to guess the number.")
 time.sleep(3.75)
-print("I am thinking of a number between 1 and " + str(p))
-print()
-time.sleep(2.75)
-print("What is your guess for " + str(p) + " points?")
-g = int(input()) ## first guess
-print()
 
 
-while 1 == 1:
-    if n == g:
-        print("Congrats! You guessed the number for " + str(p) + " points!")
-        break
-    elif n < g:
-        print("The Number is lower than " + str(g))
-        p -= int(m * random.randint(5,15))
-        m += d
-        if p < 0:
+while repeat == True:
+    m = 1 ##starting multiplier
+    p = 100 ##starting points, add input option?
+    n = random.randint(1, p) ## picks the random number
+    print("I am thinking of a number between 1 and " + str(p) + ".")
+    print()
+    time.sleep(2.75)
+    print("What is your guess for " + str(p) + " points?")
+    g = int(input()) ## first guess
+    print()
+
+
+    while 1 == 1:
+        if n == g:
+            print("Congrats! You guessed the number for " + str(p) + " points!")
+            break
+        elif n < g:
+            print("The Number is lower than " + str(g))
+            p -= int(m * random.randint(5,15))
+            m += d
+            if p < 0:
+                print("Sorry, you lose")
+                break
+            print("Guess again for " + str(p) + " points!")
+            g = int(input())
+            print()
+        elif n > g:
+            print("The Number is higher than " + str(g))
+            p -= int(m * random.randint(5,15))
+            m += d
+            if p < 0:
+                print("Sorry, you lose")
+                break
+            print("Guess again for " + str(p) + " points!")
+            g = int(input())
+            print()
+        elif p < 0:
             print("Sorry, you lose")
             break
-        print("Guess again for " + str(p) + " points!")
-        g = int(input())
+    print("The number was: " + str(n))
+    time.sleep(1)
+    print("Would you like to play again? Y/N")
+    repeatq = input()
+    if repeatq == "Y":
+        repeat = True
         print()
-    elif n > g:
-        print("The Number is higher than " + str(g))
-        p -= int(m * random.randint(5,15))
-        m += d
-        if p < 0:
-            print("Sorry, you lose")
-            break
-        print("Guess again for " + str(p) + " points!")
-        g = int(input())
-        print()
-    elif p < 0:
-        print("Sorry, you lose")
-        break
-print("The number was: " + str(n))
+    else:
+        repeat = False
+
+print("Thanks for playing!")
+    
